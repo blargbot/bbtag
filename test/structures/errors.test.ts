f858720996ef0056efa6193856e9746efd4f11f3
Danny May <dannyjmay97@gmail.com>
2019-07-03 22:42:48 +0100

    tweak errors test

--- test/structures/errors.test.ts
+++ test/structures/errors.test.ts
@@ -1,6 +1,6 @@
 // tslint:disable-next-line: no-implicit-dependencies
 import { expect } from 'chai';
-import { AggregateError, ChainedError, IStringToken, ISubtagToken, SubtagContext, SubtagError } from '../../src/structures';
+import { AggregateError, ChainedError, IStringToken, ISubtagToken, SubtagError } from '../../src/structures';
 import { MockExecutionContext } from '../test helpers/mocks';
 import { str, tag } from '../test helpers/subtag';
 
@@ -57,24 +57,25 @@ describe('class AggregateError', () => {
 });
 
 describe('class SubtagError', () => {
-    const testCases: Array<{ context: SubtagContext, token: ISubtagToken | IStringToken, message?: string, innerError: any }> = [
-        { context: new MockExecutionContext(), token: str('text'), message: undefined, innerError: undefined },
-        { context: new MockExecutionContext(), token: tag(str('text')), message: undefined, innerError: undefined },
-        { context: new MockExecutionContext(), token: str('text'), message: 'test', innerError: undefined },
-        { context: new MockExecutionContext(), token: tag(str('text')), message: 'test', innerError: undefined },
-        { context: new MockExecutionContext(), token: str('text'), message: undefined, innerError: 12345 },
-        { context: new MockExecutionContext(), token: tag(str('text')), message: undefined, innerError: 12345 },
-        { context: new MockExecutionContext(), token: str('text'), message: 'test', innerError: 12345 },
-        { context: new MockExecutionContext(), token: tag(str('text')), message: 'test', innerError: 12345 }
+    const testCases: Array<{ token: ISubtagToken | IStringToken, message?: string, innerError: any }> = [
+        { token: str('text'), message: undefined, innerError: undefined },
+        { token: tag(str('text')), message: undefined, innerError: undefined },
+        { token: str('text'), message: 'test', innerError: undefined },
+        { token: tag(str('text')), message: 'test', innerError: undefined },
+        { token: str('text'), message: undefined, innerError: 12345 },
+        { token: tag(str('text')), message: undefined, innerError: 12345 },
+        { token: str('text'), message: 'test', innerError: 12345 },
+        { token: tag(str('text')), message: 'test', innerError: 12345 }
     ];
 
-    for (const { context, token, message, innerError } of testCases) {
+    for (const { token, message, innerError } of testCases) {
         it(`should successfully construct ` +
             `${'name' in token ? '(subtag token)' : '(string token)'}` +
             `${message === undefined ? '(no message)' : ''}` +
             `${innerError === undefined ? '(no inner error)' : ''}`,
             () => {
                 // arrange
+                const context = new MockExecutionContext();
 
                 // act
                 const result = new SubtagError(context, token, message, innerError);
