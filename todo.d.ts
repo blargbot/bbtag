3ef01a36164d20fb9470871fbda71d6b75233469
Danny May <dannyjmay97@gmail.com>
2019-07-27 22:52:07 +0100

    Update todo

--- todo.d.ts
+++ todo.d.ts
@@ -1,2 +1,7 @@
 // TODO: bu.parseDuration
-// TODO: api for {ban}
+// TODO: Framework for scoped subtags - {fallback} {suppressLookup} {quiet} {reason}
+// TODO: Framework for child contexts - {exec} {execcc} {filter} {waitmessage} {waitreact}
+// TODO: Framework for performing queries, ideally without coupling to discord - {user*} {role*} {channel*}
+// TODO: Framework for serializing and deserializing contexts - {timer}
+// TODO: Framework for subtag limits
+// TODO: Framework for debugging - {debug}
